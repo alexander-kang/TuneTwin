a5dbd79c9974e33efd8bb6b19d0187e9cb768ada
alexander-kang <alexkang98767@gmail.com>
2023-09-23 22:44:04 -0500

    Made name in nav bar link back home

--- tune-twin/src/components/navBar/navBarNotSignedIn.js
+++ tune-twin/src/components/navBar/navBarNotSignedIn.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import './navBar.css'
 
 const NavBarNotSignedIn = () => {
+  const navigate = useNavigate()
+  const navHome = () => {
+      navigate("/")
+  }
+
   return (
     <nav className="navbar">
-        <div className="navbar-left">
+        <div className="navbar-left" onClick={navHome}>
           <i>TuneTwin</i>
         </div>
+        
         <div className="navbar-right">
-          <a href="http://127.0.0.1:8080/">Sign in</a>
+          <a href="http://127.0.0.1:8080/">
+            Sign in
+          </a>
         </div>
     </nav>
   )
--- tune-twin/src/components/navBar/navBarSignedIn.js
+++ tune-twin/src/components/navBar/navBarSignedIn.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import './navBar.css'
 
 const NavBarSignedIn = () => {
+  const navigate = useNavigate()
+  const navHome = () => {
+      navigate("/")
+  }
+
   return (
     <nav className="navbar">
-        <div className="navbar-left">
+        <div className="navbar-left" onClick={navHome}>
           <i>TuneTwin</i>
         </div>
     </nav>
