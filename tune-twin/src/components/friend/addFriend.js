7e2aac224441cf04a398811490fabf6159a27d53
ikim-2001 <isaackim2024@u.northwestern.edu>
2023-09-24 09:58:56 -0500

    restyled add friends box

--- tune-twin/src/components/friend/addFriend.js
+++ tune-twin/src/components/friend/addFriend.js
@@ -32,13 +32,13 @@ function AddFriend(props) {
         <div className="add-friend">
             <h2 className="friend-heading">Add Friend</h2>
             <form onSubmit={handleSubmit}>
-                <input
+                <input className='friend-input'
                     type="text"
                     value={inputText}
                     onChange={handleInputChange}
                     placeholder="Enter friend's email"
                 />
-                <button type="submit">Submit</button>
+                <button className='submit-button' type="submit">Submit</button>
             </form>
         </div>
     )
